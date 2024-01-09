8aae7419aab5b0c81a8753eaf0f12194e81a442c
samdeez <soroushsam70@gmail.com>
2024-01-09 17:39:50 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,234 @@
+import React, { useState } from "react";
+import style from "./style.css";
+
+const App = () => {
+  const [feesPercent, setFeesPercent] = useState(10);
+  const [localPrice, setlocalPrice] = useState(52000);
+  const [invest, setInvest] = useState("");
+  const [enterPrice, setEnterPrice] = useState("");
+  const [exitPrice, setExitPrice] = useState("");
+  const [leverage, setLeverage] = useState(5);
+
+  const handleFeesPercentChange = (event) => {
+    setFeesPercent(event.target.value);
+  };
+
+  const handleLocalPriceChange = (event) => {
+    setlocalPrice(event.target.value);
+  };
+
+  const handleEnterPriceChange = (event) => {
+    setEnterPrice(event.target.value);
+  };
+
+  const handleExitPriceChange = (event) => {
+    setExitPrice(event.target.value);
+  };
+
+  const handleLeverageChange = (event) => {
+    setLeverage(event.target.value);
+  };
+
+  const handleInvestChange = (event) => {
+    setInvest(event.target.value);
+  };
+
+  function levCalc() {
+    let levSum =
+      (
+        ((Number(invest) * Number(leverage)) / Number(enterPrice)) *
+        (Number(exitPrice) - Number(enterPrice))
+      ).toFixed(2) *
+      (1 - Number(feesPercent) / 100);
+
+    return levSum;
+  }
+
+  function shortCalc() {
+    let shortLiq = enterPrice / leverage;
+    shortLiq += +enterPrice;
+
+    return shortLiq.toLocaleString();
+  }
+
+  return (
+    <div className="container-wrapper">
+      <h1
+        style={{
+          //marginTop: "100px",
+          backgroundColor: "blue",
+          borderRadius: "3px",
+          padding: "10px",
+          textAlign: "center",
+          color: "whitesmoke",
+          fontFamily: "monospace",
+        }}
+      >
+        Leverage Calculator
+      </h1>
+
+      <div className="container">
+        <div className="insidecol">
+          <h2 style={{ textAlign: "center" }}>Input Trade Details:</h2>
+          <hr style={{ marginBottom: "50px" }} />
+          <div className="inputContainer">
+            <div className="inputLabel">Trade Amount:</div>
+            <input
+              type="number"
+              value={invest}
+              onChange={handleInvestChange}
+              className="input"
+              placeholder="0"
+              onFocus={() => console.log("Clicked!")}
+            />
+
+            <div className="inputSuffix">$</div>
+          </div>
+
+          <div className="inputContainer">
+            <div className="inputLabel">Entry Price:</div>
+            <input
+              type="number"
+              value={enterPrice}
+              onChange={handleEnterPriceChange}
+              className="input"
+              placeholder="0"
+            />
+            <div className="inputSuffix">$</div>
+          </div>
+
+          <div className="inputContainer">
+            <div className="inputLabel">Exit Price:</div>
+            <input
+              type="number"
+              value={exitPrice}
+              onChange={handleExitPriceChange}
+              className="input"
+              placeholder="0"
+            />
+            <div className="inputSuffix">$</div>
+          </div>
+
+          <div className="inputContainer">
+            <div className="inputLabel">USD Price:</div>
+            <input
+              type="number"
+              value={localPrice}
+              onChange={handleLocalPriceChange}
+              className="input"
+              placeholder="0"
+            />
+            <div className="inputSuffix">T</div>
+          </div>
+
+          <div className="inputContainer">
+            <div className="inputLabel">Leverage:</div>
+            <input
+              type="number"
+              value={leverage}
+              onChange={handleLeverageChange}
+              className="input"
+              placeholder="0"
+            />
+            <div className="inputSuffix">$</div>
+          </div>
+
+          <div className="inputContainer">
+            <div className="inputLabel">Fees:</div>
+            <input
+              type="number"
+              value={feesPercent}
+              onChange={handleFeesPercentChange}
+              className="input"
+              placeholder="0"
+            />
+            <div className="inputSuffix">%</div>
+          </div>
+        </div>
+
+        <div className="insidecol">
+          <p className="outputLabel">P&L:</p>
+          <p
+            className="resultgrid"
+            style={{
+              backgroundColor: "#303030",
+              color: levCalc() < 0 ? "firebrick" : "seagreen",
+            }}
+          >
+            {levCalc() > 0
+              ? "+" + levCalc().toLocaleString()
+              : levCalc().toLocaleString()}{" "}
+            $
+            <br />
+            {(levCalc() * localPrice).toLocaleString()} T
+          </p>
+          <div className="reswrapper">
+            <div className="reswrapperinner">
+              <p className="outputLabel">Entery & Exit:</p>
+              <div
+                className="resultgrid"
+                style={{
+                  backgroundColor: "gray",
+                }}
+              >
+                <span style={{ fontSize: "small" }}>{"IN:"}</span> <br />
+                <span>{(enterPrice * 1).toLocaleString()} $</span>
+                <br />
+                <hr class="solid" style={{ margin: "5px" }} />
+                <span style={{ fontSize: "small" }}>{"OUT:"}</span> <br />
+                <span>{(exitPrice * 1).toLocaleString()} $</span>
+              </div>
+            </div>
+            <div className="reswrapperinner">
+              <p className="outputLabel">Liquid price:</p>
+              <div
+                className="resultgrid"
+                style={{
+                  backgroundColor: "rgb(178, 130, 10)",
+                }}
+              >
+                <span style={{ fontSize: "small" }}>Long ⬆</span>
+                <br />
+                <span>
+                  {(enterPrice - enterPrice / leverage).toLocaleString()} $
+                </span>
+                <br />
+                <hr class="solid" style={{ margin: "5px" }} />
+                <span style={{ fontSize: "small" }}>Short ⬇</span>
+                <br />
+                <span>{shortCalc()} $</span>
+              </div>
+            </div>
+          </div>
+
+          <p className="outputLabel">Margin:</p>
+
+          <div className="pl">
+            <p className="resultgrid">
+              {(invest * 1).toLocaleString()} $
+              <br />
+              {(invest / enterPrice).toFixed(6)} ₿
+              <br />
+              {(invest * localPrice).toLocaleString()} T
+            </p>
+            <p className="outputLabel">Size:</p>
+            <p
+              className="resultgrid"
+              style={{
+                backgroundColor: "darkslategray",
+              }}
+            >
+              {(invest * leverage).toLocaleString()} $
+              <br />
+              {((invest / enterPrice) * leverage).toFixed(6)} ₿
+              <br />
+              {(invest * leverage * localPrice).toLocaleString()} T
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
